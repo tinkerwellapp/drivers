9df9da9a9ddda19a27ef196e470a97ab162c07d2
edalzell <erin@thedalzells.org>
2020-01-28 08:15:55 -0800

    simplify and include Laravel menu as well.

--- src/Drivers/StatamicTinkerwellDriver.php
+++ src/Drivers/StatamicTinkerwellDriver.php
@@ -5,35 +5,32 @@ use Tinkerwell\ContextMenu\OpenURL;
 use Tinkerwell\ContextMenu\SetCode;
 use Tinkerwell\ContextMenu\Submenu;
 
-class StatamicTinkerwellDriver extends TinkerwellDriver
+class StatamicTinkerwellDriver extends LaravelTinkerwellDriver
 {
     public function canBootstrap($projectPath)
     {
-        return file_exists($projectPath . '/vendor/statamic/cms/src/Statamic.php') &&
-        file_exists($projectPath . '/please');
-    }
-
-    public function bootstrap($projectPath)
-    {
-        require_once $projectPath . '/vendor/autoload.php';
-
-        $app = require_once $projectPath . '/bootstrap/app.php';
-
-        $kernel = $app->make(\Illuminate\Contracts\Console\Kernel::class);
-
-        $kernel->bootstrap();
+        return file_exists($projectPath . 'config/statamic/system.php');
     }
 
     public function contextMenu()
     {
-        return [
+        return array_merge(parent::contextMenu(), [
             Label::create('Detected Statamic v' . \Statamic\Statamic::version()),
 
-            Submenu::create('Artisan', collect(Artisan::all())->map(function ($command, $key) {
-                return SetCode::create($key, "Artisan::call('" . $key . "', []);\nArtisan::output();");
-            })->values()->toArray()),
+            Submenu::create(
+                'Please',
+                collect(Artisan::all())
+                    ->filter(function ($command, $key) {
+                        $length = strlen('statamic');
+
+                        return (substr($key, 0, $length) === 'statamic');
+                    })
+                    ->map(function ($command, $key) {
+                        return SetCode::create($key, "Artisan::call('" . $key . "', []);\nArtisan::output();");
+                    })->values()->toArray()
+            ),
 
             OpenURL::create('Statamic Docs', 'https://statamic.dev'),
-        ];
+        ]);
     }
 }
